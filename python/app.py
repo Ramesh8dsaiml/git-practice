91cb7f6c961ff7635df27d5372d99378d603b36e
Ramesh8dsaiml <ramesh879133867@gmail.com>
2026-09-16 23:48:19 +0530

    add line

--- python/app.py
+++ python/app.py
@@ -4,5 +4,5 @@ print("This is a sample Python application.")
 print("Hello from conflict branch")
 print("Hello from main branch")
 print("Change made on GitHub")
-
-
+print("This is a temporary change")
+print("This is a change made in the local repository")
